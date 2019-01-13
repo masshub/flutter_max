8916f0c8b67c8bad29418cf8cea3eec0e92f7f2c
max <2416227158@qq.com>
2019-01-13 22:39:14 +0800

    34567

--- lib/page/login_page.dart
+++ lib/page/login_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
 
 ///
 /// Created by Maker on 2019/1/11.
@@ -30,21 +31,14 @@ class _LoginState extends State<Login> {
   }
 
   void _saveLoginInformation() {
+
     if (loginFormKey.currentState.validate()) {
       loginFormKey.currentState.save();
       debugPrint('account : $account');
       debugPrint('password : $password');
       if (account == 'max' && password == '123456') {
-//        showSnackBar(SnackBar(content: Text('登陆中.........')));
-
-
-//        Scaffold.of(context)
-//            .showSnackBar(SnackBar(content: Text('Logining.......')));
         Navigator.pushNamed(context, '/');
-      } else {
-        Scaffold.of(context).showSnackBar(
-            SnackBar(content: Text('account or password is wrong ')));
-      }
+      } else {}
     } else {
       setState(() {
         autoValidate = true;
@@ -52,86 +46,148 @@ class _LoginState extends State<Login> {
     }
   }
 
+  TextEditingController _accountController = TextEditingController();
+  TextEditingController _passwordController = TextEditingController();
+
+  void _clearAccount() {
+    _accountController.clear();
+  }
+
+  void _clearPassword() {
+    _passwordController.clear();
+  }
+
+  FocusNode secondTextFieldNode = FocusNode();
+
   @override
   Widget build(BuildContext context) {
     return Form(
       key: loginFormKey,
       child: Container(
         color: Colors.lightBlueAccent,
-        child: Center(
-          child: SizedBox(
-            height: 300.0,
-            child: Card(
-              color: Colors.white,
-              elevation: 10.0,
-              shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.all(Radius.circular(10.0))),
-              margin: EdgeInsets.only(right: 46.0, left: 46.0),
-              child: Padding(
-                padding: EdgeInsets.all(16.0),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: <Widget>[
-                    TextFormField(
-                      decoration: InputDecoration(
-                        labelText: '手机号码',
-                        labelStyle:
-                            TextStyle(fontSize: 14, color: Colors.grey[600]),
-                        hintText: '请输入手机号码',
-                        hintStyle:
-                            TextStyle(fontSize: 12, color: Colors.grey[400]),
-                        helperText: '',
-                        contentPadding:
-                            EdgeInsets.only(top: 16.0, bottom: 10.0),
-                      ),
-                      autovalidate: autoValidate,
-                      onSaved: (value) {
-                        account = value;
-                      },
-                      validator: _validatorAccount,
-                    ),
-                    TextFormField(
-                      decoration: InputDecoration(
-                        labelText: '密码',
-                        labelStyle:
-                            TextStyle(fontSize: 14, color: Colors.grey[600]),
-                        hintText: '请输入密码',
-                        hintStyle:
-                            TextStyle(fontSize: 12, color: Colors.grey[400]),
-                        helperText: '',
-                        contentPadding: EdgeInsets.only(top: 6.0, bottom: 10.0),
-                      ),
-                      autovalidate: autoValidate,
-                      obscureText: true,
-                      validator: _validatorPassword,
-                      onSaved: (value) {
-                        password = value;
-                      },
-                    ),
-                    SizedBox(
-                      height: 16.0,
-                    ),
-                    Container(
-                      width: double.infinity,
-                      child: RaisedButton(
-                          color: Colors.deepOrange,
-                          shape: RoundedRectangleBorder(
-                              borderRadius:
-                                  BorderRadius.all(Radius.circular(4.0))),
-                          child: Text(
-                            '登陆',
-                            style: TextStyle(color: Colors.white),
+        child: Stack(
+          alignment: Alignment.center,
+          children: <Widget>[
+            Align(
+              alignment: Alignment.topCenter,
+              child: Image.asset(
+                'assets/images/logo.jpg',
+                fit: BoxFit.fitWidth,
+              ),
+            ),
+            SizedBox(
+              height: 300.0,
+              child: Card(
+                color: Colors.white,
+                elevation: 10.0,
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(10.0))),
+                margin: EdgeInsets.only(right: 46.0, left: 46.0),
+                child: Padding(
+                  padding: EdgeInsets.all(16.0),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Stack(
+                        alignment: Alignment.centerRight,
+                        children: <Widget>[
+                          TextFormField(
+                            controller: _accountController,
+                            keyboardType: TextInputType.phone,
+                            onEditingComplete: () =>
+                                FocusScope.of(context).requestFocus(secondTextFieldNode),
+                            maxLength: 11,
+                            maxLines: 1,
+                            decoration: InputDecoration(
+                              labelText: '手机号码',
+                              labelStyle: TextStyle(
+                                  fontSize: 14, color: Colors.grey[600]),
+                              hintText: '请输入手机号码',
+                              hintStyle: TextStyle(
+                                  fontSize: 12, color: Colors.grey[400]),
+                              helperText: '',
+                              contentPadding:
+                                  EdgeInsets.only(top: 26.0, bottom: 4.0),
+                            ),
+                            cursorColor: Colors.lightBlueAccent,
+                            cursorRadius: Radius.circular(16.0),
+                            cursorWidth: 16.0,
+                            autovalidate: autoValidate,
+                            onSaved: (value) {
+                              account = value;
+                            },
+                            validator: _validatorAccount,
                           ),
-                          elevation: 0.0,
-                          onPressed: () {
-                            _saveLoginInformation();
-                          }),
-                    ),
-                  ],
+                          IconButton(
+                              onPressed: _clearAccount,
+                              icon: Icon(
+                                Icons.cancel,
+                                color: Colors.grey[400],
+                              )),
+                        ],
+                      ),
+                      Stack(
+                        alignment: Alignment.centerRight,
+                        children: <Widget>[
+                          TextFormField(
+                            controller: _passwordController,
+                            focusNode: secondTextFieldNode,
+                            keyboardType: TextInputType.text,
+                            maxLines: 1,
+                            decoration: InputDecoration(
+                              labelText: '密码',
+                              labelStyle: TextStyle(
+                                  fontSize: 14, color: Colors.grey[600]),
+                              hintText: '请输入密码',
+                              hintStyle: TextStyle(
+                                  fontSize: 12, color: Colors.grey[400]),
+                              helperText: '',
+                              contentPadding:
+                                  EdgeInsets.only(top: 6.0, bottom: 10.0),
+                            ),
+                            autovalidate: autoValidate,
+                            obscureText: true,
+                            validator: _validatorPassword,
+                            onSaved: (value) {
+                              password = value;
+                            },
+                          ),
+                          IconButton(
+                              onPressed: _clearPassword,
+                              icon: Icon(
+                                Icons.cancel,
+                                color: Colors.grey[400],
+                              )),
+                        ],
+                      ),
+                      SizedBox(
+                        height: 16.0,
+                      ),
+                      Container(
+                        width: double.infinity,
+                        child: RaisedButton(
+                            color: Colors.deepOrange,
+                            shape: RoundedRectangleBorder(
+                                borderRadius:
+                                    BorderRadius.all(Radius.circular(4.0))),
+                            child: Text(
+                              '登陆',
+                              style: TextStyle(color: Colors.white),
+                            ),
+                            elevation: 0.0,
+                            onPressed: () {
+
+                              _saveLoginInformation();
+                            }),
+                      ),
+
+                    ],
+                  ),
                 ),
               ),
             ),
-          ),
+            SpinKitCircle(color: Colors.deepOrange),
+          ],
         ),
       ),
     );
